11b220adc0b83316074b9d3f9fbd046b4dfc6534
agentbenedict <109691123+agentbenedict@users.noreply.github.com>
2022-07-20 11:18:46 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// alert("Welcome to Hi-Tel, get the chance to dowload at least 6mb music for free");
+// console.log('hello world');
+// const slices=10 ;
+// const children= 2;
+// const amount=slices/children;
+//  console.log(amount);
+const surname="Agent";
+console.log(surname);
